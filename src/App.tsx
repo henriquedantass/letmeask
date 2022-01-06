75ea7fab8afa3536ed282c50cc98071afe7ddf11
Henrique <henrique@hotmail.ph>
2022-01-06 16:56:35 -0300

    [UPDATE] createroom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { Home } from "./pages/Home";
 
 import "../src/styles/global.scss";
+import { NewRoom } from "./pages/NewRoom";
 
 function App() {
-  return <Home />;
+  return <NewRoom />;
 }
 
 export default App;
---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,34 @@
+import ilustrationImg from "../assets/images/illustration.svg";
+import logoImg from "../assets/images/logo.svg";
+import logoGoogle from "../assets/images/google-icon.svg";
+
+import "../styles/auth.scss";
+import { Button } from "../components/Button";
+
+export function NewRoom() {
+  return (
+    <div id="page-auth">
+      <aside>
+        <img
+          src={ilustrationImg}
+          alt="Ilustração simbolizando perguntas e respostas"
+        />
+        <strong>Crie salas de Q&amp;A ao-vivo</strong>
+        <p>Tire as suas dúvidas em tempo real</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Letmeask" />
+          <h2>Criar uma nova sala </h2>
+          <form>
+            <input type="text" placeholder="Nome da sala" />
+            <Button type="submit">Criar sala</Button>
+          </form>
+          <p>
+            Quer entrar em uma sala existente? <a href="#">clique aqui</a>
+          </p>
+        </div>
+      </main>
+    </div>
+  );
+}
